e6df67b327b179af5ac25e7556cf762838c9925d
abdullah <abdullah.pichoo@gmail.com>
2023-10-17 01:36:11 +0500

    update next.config for cors

--- next.config.js
+++ next.config.js
@@ -15,21 +15,20 @@ const nextConfig = {
   async headers() {
     return [
       {
-        source: "/api/:path*",
+        source: "/api/(.*)",
+        // Headers
         headers: [
-          { key: "Access-Control-Allow-Credentials", value: "true" },
           {
             key: "Access-Control-Allow-Origin",
-            value: process.env.NEXT_PUBLIC_BASE_API_URL,
+            value: "*",
           },
           {
             key: "Access-Control-Allow-Methods",
-            value: "GET,DELETE,PATCH,POST,PUT",
+            value: "GET, POST, PUT, DELETE, OPTIONS",
           },
           {
             key: "Access-Control-Allow-Headers",
-            value:
-              "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version",
+            value: "Content-Type, Authorization",
           },
         ],
       },
