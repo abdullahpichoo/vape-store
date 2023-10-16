ac6ed2afb733e0425ed0fb57cde32998815c7ecc
abdullah <abdullah.pichoo@gmail.com>
2023-10-17 02:17:04 +0500

    new implementation for admin check 2

--- backend/middleware/index.ts
+++ backend/middleware/index.ts
@@ -3,11 +3,17 @@ import { decode, getToken } from "next-auth/jwt";
 
 export const isAdmin = async (req: NextRequest) => {
   const token = await getToken({ req });
-  console.log("JWT", token);
+  const sessionCookie = req.cookies.get("next-auth.session-token")?.value;
+  const session = await decode({
+    token: sessionCookie,
+    secret: process.env.NEXTAUTH_SECRET as string,
+  });
+
+  if (!session) return false;
 
   if (!token) return false;
 
-  if (token.role === "admin") return true;
+  if (token.role === "admin" || session.role === "admin") return true;
 
   return false;
 };
