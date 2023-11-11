292b77475771571d29b7531658ee69143c618199
abdullah <abdullah.pichoo@gmail.com>
2023-11-11 23:00:33 +0500

    added age verify

--- app/page.tsx
+++ app/page.tsx
@@ -9,6 +9,7 @@ export default function Home() {
   return (
     <>
       <div className="flex flex-col gap-10 relative">
+        <AgeVerify />
         <HeroSection />
         <BestSelling />
         <Features />
